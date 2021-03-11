671c31cba3647b42df9c86645dd2daba49a2a28b
finderfinder <49021865+finderfinder@users.noreply.github.com>
2021-03-11 18:15:11 +0800

    Add files via upload

---
+++ Python/fizzbuzz.py
@@ -0,0 +1,13 @@
+'''
+Write a Python program which iterates the integers from 1 to 50. For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+'''
+
+for i in range(101):
+    output = ""
+    if i % 3 == 0:
+        output = output + "Fizz"
+    if i % 5 == 0:
+        output = output + "Buzz"
+    if output == "":
+        output = str(i)
+    print (output)
