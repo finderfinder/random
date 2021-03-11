9bd927bcd35b15211e90b55c2b6d2f4f9db893cc
finderfinder <49021865+finderfinder@users.noreply.github.com>
2021-03-11 18:17:08 +0800

    Update fizzbuzz.py

--- Python/fizzbuzz.py
+++ Python/fizzbuzz.py
@@ -1,8 +1,8 @@
 '''
-Write a Python program which iterates the integers from 1 to 50. For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+Write a Python program which iterates the integers from 1 to 100. For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 '''
 
-for i in range(101):
+for i in range(1,101):
     output = ""
     if i % 3 == 0:
         output = output + "Fizz"
